627146f12b038b91847f8649a22afbd2013687e2
Vlad vilams <vladkinall@gmail.com>
2023-05-03 18:09:05 +1000

    Добавил поиск по дате

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Window:
         self.root.resizable(resizable[0], resizable[1])
         self.choice = IntVar(value=0)
         self.textVar= StringVar(value="")
+        self.textVar2 = StringVar(value="")
+        self.textVar3 = StringVar(value="")
 
     #запуск приложения
     def run(self):
@@ -35,13 +37,15 @@ class Window:
 
             for event in events:
                 if (self.textVar.get() in f"{event.EventCategory}") or (self.textVar.get() in f"{event.TimeGenerated}") or (self.textVar.get() in f"{event.SourceName}") or (self.textVar.get() in f"{event.EventID}") or (self.textVar.get() in f"{event.EventType}") or (self.textVar.get() in f"{event.StringInserts}"):
-                    self.text.insert('insert', f"{event.EventCategory}\n")
-                    self.text.insert('insert', f"{event.TimeGenerated}\n")
-                    self.text.insert('insert', f"{event.SourceName}\n")
-                    self.text.insert('insert', f"{event.EventID}\n")
-                    self.text.insert('insert', f"{event.EventType}\n")
-                    self.text.insert('insert', f"{event.StringInserts}\n")
-                    self.text.insert('insert', f"{'=' * 50}\n")
+                    if  (self.dateSearch(self.textVar2.get(),1) <= self.dateSearch(event.TimeGenerated) <= self.dateSearch(self.textVar3.get(),2)):
+                        self.text.insert('insert', f"{event.EventCategory}\n")
+                        self.text.insert('insert', f"{event.TimeGenerated}\n")
+                       # print(self.dateSearch(event.TimeGenerated))
+                        self.text.insert('insert', f"{event.SourceName}\n")
+                        self.text.insert('insert', f"{event.EventID}\n")
+                        self.text.insert('insert', f"{event.EventType}\n")
+                        self.text.insert('insert', f"{event.StringInserts}\n")
+                        self.text.insert('insert', f"{'=' * 50}\n")
         # for event in self.varIvent:
         #
         #     self.text.insert('insert', f"{event.ComputerName}\n")
@@ -52,6 +56,16 @@ class Window:
 
         self.text.configure(state='disabled')
         self.text.pack(fill='both', expand=True)
+
+    def dateSearch(self,date,flag=0):
+        date = f"{date}"
+        if (date != ''):
+            print(int(date.replace('-', '')[0:8]))
+            return int(date.replace('-','')[0:8])
+        elif (date=='' and flag == 1):
+            return 0
+        elif (date=='' and flag == 2):
+            return 100000000
 
 
 
@@ -71,8 +85,11 @@ class Window:
     def drowWidgets(self):
         frameSelect = Frame(self.root)
         frameSearch = Frame(self.root)
+        frameSearch2 = Frame(self.root)
+
         frameSelect.pack()
         frameSearch.pack()
+        frameSearch2.pack()
         Radiobutton(frameSelect, text="System", width=10, height=3, variable=self.choice, value=0).pack(side=LEFT)
         Radiobutton(frameSelect, text="Application", width=10, height=3, variable=self.choice, value=1).pack(side=LEFT)
         Radiobutton(frameSelect, text="Security", width=10, height=3, variable=self.choice, value=2).pack(side=LEFT)
@@ -81,6 +98,9 @@ class Window:
         self.entry = Entry(frameSearch,textvariable=self.textVar).pack(side=LEFT)
         self.button = Button(frameSearch, text="search", command=self.update_text).pack(side=LEFT)
 
+        self.entry2 = Entry(frameSearch2, textvariable=self.textVar2).pack(side=LEFT)
+        self.entry3 = Entry(frameSearch2, textvariable=self.textVar3).pack(side=LEFT)
+        self.button = Button(frameSearch2, text="search", command=self.update_text).pack(side=LEFT)
 
     #обновление списка логов
     def update_text(self):
